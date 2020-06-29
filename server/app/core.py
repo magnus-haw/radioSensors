e2f8152dc27467cd035402078c21c5474e133c7f
Leo Lin <mml0816@gmail.com>
2020-06-29 11:27:17 -0700

    add db

---
+++ server/app/core.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, resource={r"/*": {"origins": "*"}})
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['RADIO_SENSOR']
+
+api = Api(app)
+db = SQLAlchemy(app)
+
+import app.endpoints
+
+db.create_all()
+db.session.commit()
