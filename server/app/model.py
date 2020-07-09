e41859239c200734b2bd2c4684a34059630d90c4
Leo Lin <mml0816@gmail.com>
2020-07-09 11:31:23 -0700

    add polymorphic

--- server/app/model.py
+++ server/app/model.py
@@ -3,7 +3,15 @@ from sqlalchemy import Column, String, Integer, Float, DateTime
 from flask_restful import reqparse
 from app import db
 
-class SensorData(db.Model):
+# Defining a schema for all sensor data
+# class SensorData(db.Model):
+#     id          = Column(Integer,       nullable=False, primary_key=True)
+#     data_type   = Column(String,        nullable=False)
+#     data        = Column(Float,         nullable=False)
+#     timestamp   = Column(DateTime,      nullable=False, default=datetime.datetime.utcnow)
+
+# Defining a schema for each category of sensor data
+class TempData(db.Model):
     id          = Column(Integer,       nullable=False, primary_key=True)
     data        = Column(Float,         nullable=False)
     timestamp   = Column(DateTime,      nullable=False, default=datetime.datetime.utcnow)
@@ -18,11 +26,11 @@ def add_data(data):
 
     data = parser.parse_args()
 
-    db.session.add(SensorData(**data))
+    db.session.add(TempData(**data))
     db.session.commit()
 
 def list():
-    return SensorData.query.all()
+    return [x.as_dict() for x in TempData.query.all()]
 
 db.create_all()
 db.session.commit()
