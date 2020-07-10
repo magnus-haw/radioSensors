fe75c7ee6b03033ec878c50d7b0d61e2b7105aa2
Leo Lin <mml0816@gmail.com>
2020-07-10 10:06:54 -0700

    add cache debug message

--- server/sensor/main.py
+++ server/sensor/main.py
@@ -6,5 +6,6 @@ FLASK_URL = '10.0.0.14/temperature'
 if __name__ == '__main__':
     sensor = Sensor()
     for cache in sensor.loop(): 
+        print(cache)
         if (cache):
             requests.post(FLASK_URL, json=cache)
