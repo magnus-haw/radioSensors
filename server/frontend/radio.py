09fafaaf2dd8aac3a77dff2c815dbaec6282bf09
Leo Lin <mml0816@gmail.com>
2020-07-23 14:52:15 -0700

    resolve import

--- server/frontend/radio.py
+++ server/frontend/radio.py
@@ -82,3 +82,26 @@ class RadioBonnet:
                     self.display.show()
                     time.sleep(INTERVAL)
                 except json.decoder.JSONDecodeError: pass
+
+from frontend import db
+from frontend.models import Sensor, Point
+from .radio import RadioBonnet
+
+radio = RadioBonnet()
+CACHE_LEVEL = 5
+
+def init_radio(experiment):
+    cache = []
+
+    for data in radio.listen():
+        sensor = Sensor.filter_by(name=data['name']).first()
+        if not sensor:
+            sensor = Sensor(name=data['name'], unit=data['unit'])
+            db.session.add(sensor)
+        point = Point(
+            data=data['value'],
+            sensor=sensor,
+            experiment=experiment
+        )
+        db.session.add(point)
+        db.session.commit()
