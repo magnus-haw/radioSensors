db2b6668fd7dbe7087e8d3a44fafae930271d2ef
Leo Lin <mml0816@gmail.com>
2020-07-28 13:42:07 -0700

    try application factory

--- server/frontend/__init__.py
+++ server/frontend/__init__.py
@@ -3,22 +3,33 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 import threading
 
-app = Flask(__name__, instance_relative_config=False)
-app.config.from_object('config.Config')
-db = SQLAlchemy(app)
-sess = Session(app)
+# Globally accessible libraries
+db = SQLAlchemy()
+sess = Session()
 
-from . import models
-db.create_all() 
+def create_app():
+    """Initialize the core application."""
+    app = Flask(__name__, instance_relative_config=False)
+    app.config.from_object('config.Config')
+    
+    # Initialize Plugins
+    db.init_app(app)
+    sess.init_app(app)
+    
+    with app.app_context():
+        from . import routes  # Import routes
+        from . import admin
+        db.create_all()       # Create sql tables
 
-from . import routes
-from . import admin
+        
+        from .radio import RadioBonnet
+        radio = RadioBonnet()
+        session['radio'] = radio
+        sensor_thread = threading.Thread(target=radio.listen)
+        sensor_thread.start()
 
-from .radio import RadioBonnet
-radio = RadioBonnet()
-session['radio'] = radio
-sensor_thread = threading.Thread(target=radio.listen)
-sensor_thread.start()
+        # Register Blueprints
+#        app.register_blueprint(auth.auth_bp)
+#        app.register_blueprint(admin.admin_bp)
 
-# app.register_blueprint(auth.auth_bp)
-# app.register_blueprint(admin.admin_bp)
+        return app
