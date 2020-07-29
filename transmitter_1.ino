e58573b0665dfc01b707e6056d57b8ead38f9212
audtree <audrey8blue@gmail.com>
2020-07-29 14:29:02 -0700

    Sends temperature, but is not received.

--- transmitter_1.ino
+++ transmitter_1.ino
@@ -1,25 +1,38 @@
 #include <SPI.h>
 #include <Wire.h>
-#include <ArduinoJson.h>
 #include <RH_RF69.h>          // RFM
 #include <Adafruit_GFX.h>     // OLED
 #include <Adafruit_SSD1306.h> // OLED
 #include <RHReliableDatagram.h>
+#include <avr/dtostrf.h>
 
 // RFM
 #define RF69_FREQ 915.0
 #define RFM69_CS  8
 #define RFM69_INT 3
 #define RFM69_RST 4
+#define LED       13
+
+// Thermistor
+#define THERMISTORPIN A0          // which analog pin to connect
+#define THERMISTORNOMINAL 10000   // resistance at 25 degrees C
+#define TEMPERATURENOMINAL 25     // temp. for nominal resistance (almost always 25 C)
+#define NUMSAMPLES 5              // how many samples to take and average, more takes longer, but is more 'smooth'
+#define BCOEFFICIENT 3950         // The beta coefficient of the thermistor (usually 3000-4000)
+#define SERIESRESISTOR 10000      // the value of the 'other' resistor
 
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
+int samples[NUMSAMPLES];
 
 void setup() {
   Serial.begin(115200);
   while (!Serial) { delay(1); }
+  analogReference(AR_EXTERNAL);
 
-  Serial.println("Test serial print 1.");
+  Serial.println("Temperature Sensor 1!");
   Serial.println();
+
+  pinMode(LED, OUTPUT); // do you need this?
 
   pinMode(RFM69_RST, OUTPUT);                 // Manually prepare output pin
   digitalWrite(RFM69_RST, LOW);
@@ -42,20 +55,53 @@ void setup() {
   rf69.setTxPower(20, true);
 }
 
+float temp () {
+  uint8_t i;
+  float average;
+
+  // take N samples in a row, with a slight delay
+  for (i=0; i< NUMSAMPLES; i++) {
+   samples[i] = analogRead(THERMISTORPIN);
+   delay(10);
+  }
+
+  // average all the samples out
+  average = 0;
+  for (i=0; i< NUMSAMPLES; i++) {
+     average += samples[i];
+  }
+  average /= NUMSAMPLES;
+
+  // convert the value to resistance
+  average = 1023 / average - 1;
+  average = SERIESRESISTOR / average;
+
+  float steinhart;
+  steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
+  steinhart -= 273.15;                         // convert to C
+
+  return steinhart;
+
+}
+
 void loop()
 {
-  delay(5000);
-  const int CAPACITY = JSON_OBJECT_SIZE(1); // only one pair (see below)
-  StaticJsonDocument<CAPACITY> data;
+  delay(1000);  // Wait 1 second between transmits, could also 'sleep' here!
 
-  JsonObject object = data.to<JsonObject> (); // create an object, JsonObjects come in key-value pairs
-  object["hello"] = "world"; // assigning pair
+  char datapacket[20]; // Buffer big enough for 7-character float, can send ~40/50 characters
+  dtostrf(temp (), 6, 2, datapacket); // Leave room for too large numbers!
 
-  char buf[128]; // creates buffer containing at most 128 bytes
-  serializeJson(data, buf); // serialize the object and send the result to the buffer (like a loading pad)
+  char comma[2] = ","; // 2 bc it's null-terminated
+  strcat(datapacket, comma);
 
-  Serial.println("Sending packet!");
+  Serial.print("Sending data: "); Serial.println(datapacket);
 
-  rf69.send((uint8_t *)buf, sizeof(buf)); // send info!
-  rf69.waitPacketSent(); // wait until done transmitting
+  // Send a message!
+  rf69.send((uint8_t *)datapacket, strlen(datapacket));
+  rf69.waitPacketSent();
+
 }
